fbdd260b296517891f901b2d0485f806365c375e
iGhoneim <Ghoneim@Outlook.com>
2016-07-21 11:13:58 +0300

    clickButton
    clickable button with toast

--- app/src/main/java/info/Ghoneim/Clickable/MainActivity.java
+++ app/src/main/java/info/Ghoneim/Clickable/MainActivity.java
@@ -2,12 +2,26 @@ package info.Ghoneim.Clickable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = (Button) findViewById(R.id.clickButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "ma bora7a :@", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
